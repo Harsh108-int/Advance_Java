a59396f5c81e78bc2ab313d21caf96cec341ee7f
Harsh666-dev <pandeharshavardhan@gmail.com>
2024-12-31 17:37:00 +0530

    Insert Data in Data Base

---
+++ DAY_1/Insert_Data.java
@@ -0,0 +1,45 @@
+import java.sql.*;
+public class Insert_Data {
+    public static void main(String[] args) {
+        //jdbc url
+        String jdbc = "jdbc:mysql://localhost:3307/customer";
+        //user
+        String user = "root";
+        //password
+        String password = "Harsh$1000Pande";
+
+        //declare Connection Object
+        Connection c1 = null;
+        //declare Statement Object
+        Statement s1 = null;
+        try {
+            //establish a connection
+            c1 = DriverManager.getConnection(jdbc,user,password);
+            System.out.println("Successfully Connected.....");
+            //create Statement Object
+            s1 = c1.createStatement();
+            String query = "INSERT INTO user (name,email,password) VALUES ('Harshvardhan','pande@gmail.com','Harsh$1000')";
+            //use for Execute SQL Query
+            int i = s1.executeUpdate(query);
+            System.out.println(i + "\tRecord is Inserted Successfully....");
+        }catch (SQLException s)
+        {
+            System.err.println(s);
+        }finally {
+            try {
+                //this is main step
+                //close all resources
+                if (c1 != null) {
+                    c1.close();
+                }
+                if (s1 != null)
+                {
+                    s1.close();
+                }
+            }catch (SQLException s2)
+            {
+                System.err.println(s2);
+            }
+        }
+    }
+}
