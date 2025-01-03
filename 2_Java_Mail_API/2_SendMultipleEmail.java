f681cfc86ffb1fb8704a2d92f5fac7c97169f512
Harsh666-dev <pandeharshavardhan@gmail.com>
2025-01-03 18:14:11 +0530

    Send Multiple Email At Same Time

---
+++ 2_Java_Mail_API/2_SendMultipleEmail.java
@@ -0,0 +1,72 @@
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class SendMultipleEmail {
+    public static void main(String[] args) {
+        //my email
+        final String sender_email = "h87539994@gmail.com";
+
+        //my app password
+        final String password = "shso suhu dzxh lbtb";
+
+        //port number
+        String port = "587";
+
+        //host is smtp.gmail.com
+        String host = "smtp.gmail.com";
+
+        //store key-value pair of smtp server detail
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host",host);
+        properties.put("mail.smtp.port",port);
+        properties.put("mail.smtp.auth","true");
+        properties.put("mail.smtp.starttls.enable","true");
+
+        //create session object
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(sender_email,password);
+            }
+        });
+
+        //debug mode for see log detail
+        session.setDebug(true);
+
+        String subject = "Hi My Name is Harshvardhan Pande";
+        String body = "Ignore this Message.....";
+
+        try {
+            //create MimeMessage Class Object & Constructor Take Session Object as Argument
+            MimeMessage mimeMessage = new MimeMessage(session);
+
+            //set Sender Email Address
+            mimeMessage.setFrom(sender_email);
+
+            //create a array to store multiple Recipient
+            String[] recipient_email = {"pandeharshavardhan@gmail.com","harshavardhanpande99@gmail.com"};
+
+            //set Multiple Recipient using setRecipient method and we separate address with ',' using String.join()
+            mimeMessage.setRecipients(Message.RecipientType.TO, String.join(",",recipient_email));
+
+            //set subject
+            mimeMessage.setSubject(subject);
+
+            //set body
+            mimeMessage.setText(body);
+
+            //send email using Transport class with static method send
+            Transport.send(mimeMessage);
+            System.out.println("Successfully Send Email......");
+
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
